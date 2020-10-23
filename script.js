667264a109adc83eb3fcd1df77cc7debad787ea1
jamesosull16 <jamesosull16@gmail.com>
2020-10-22 21:33:21 -0600

    created rows in html, function for color coded time, changed the color for past, present, future

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 //VARIABLE DECLATION
-
-var currentDay = document.querySelector("#currentDay");
-var date = null;
-
-
-//FUNCTIONS
-
-//updating the current day and time
-var update = function () {
-    //using moment() to retrieve current day and time
-  (date = moment()),
-    currentDay.html(date.format("dddd, MMM, Do YYYY, h:mm:ss a"));
-};
-//setting the var currentDay to the id and updating the time every second
 $(document).ready(function () {
-  currentDay = $("#currentDay");
-  //calling the update function
+  var saveBtn = $(".saveBtn");
+  var description = $(".description");
+
+  //FUNCTIONS
+
+  //updating the current day and time
+  update = function () {
+    //using moment() to retrieve current day and time
+    $("#currentDay").html(moment().format("dddd, MMM, Do YYYY, h:mm:ss a"));
+  };
+  //calling the update function and setting the interval to every second
   update();
   setInterval(update, 1000);
+
+  //function for different colors dependent upon what time
+
+  var hours = date.getHours();
+  var htmlTimeBlock;
+  $(".description").each(function () {
+    htmlTimeBlock = parseInt($(this).attr("data-time"));
+    if (hours > htmlTimeBlock) {
+      $(this).addClass("past");
+    } else if (hours < htmlTimeBlock) {
+      $(this).addClass("future");
+    } else {
+      $(this).addClass("present");
+    }
+  });
+
+  //..saveBtn - event listener to save to local storage once clicked
+  $(".saveBtn").on("click", function () {});
 });
-
-
